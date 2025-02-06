5344fd1d8fa4172cc2569607b7cb361c3243c080
1625-CODE-2 <team1625@gmail.com>
2025-02-06 17:23:13 -0600

    Arm work from another day

--- src/main/java/frc/robot/subsystems/superstructure/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/superstructure/arm/Arm.java
@@ -1,3 +1,65 @@
 package frc.robot.subsystems.superstructure.arm;
 
-public class Arm {}
+import java.util.function.DoubleSupplier;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+
+import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.superstructure.elevator.Elevator.ElevatorState;
+import frc.robot.subsystems.superstructure.elevator.ElevatorIO;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+public class Arm {
+    private final ArmIO io;
+    private final ArmIOInputsAutoLogged inputs = new ArmIoInputsAutoLogged();
+    private final ArmVisualizer visualizer = new ArmVisualizer();
+    // private static final LoggedTunableNumber kP =
+    //     new LoggedTunableNumber("Arm/kP", gains.kP());
+    // private static final LoggedTunableNumber kI =
+    //     new LoggedTunableNumber("Arm/kI", gains.kI());
+    // private static final LoggedTunableNumber kD =
+    //     new LoggedTunableNumber("Arm/kD", gains.kD());
+    // private static final LoggedTunableNumber kS =
+    //     new LoggedTunableNumber("Arm/kS", gains.ffkS());
+    // private static final LoggedTunableNumber kV =
+    //     new LoggedTunableNumber("Arm/kV", gains.ffkV());
+    // private static final LoggedTunableNumber kA =
+    //     new LoggedTunableNumber("Arm/kA", gains.ffkA());
+    // private static final LoggedTunableNumber kG =
+    //     new LoggedTunableNumber("Arm/kG", gains.ffkG());
+    // private static final LoggedTunableNumber cruiseV =
+    //     new LoggedTunableNumber("Arm/cruiseV", cruiseVelocity);
+    // private static final LoggedTunableNumber cruiseA =
+    //     new LoggedTunableNumber("Arm/cruiseA", cruiseAcceleration);
+    // private static final LoggedTunableNumber cruiseJ =
+    //     new LoggedTunableNumber("Arm/cruiseJ", cruiseJerk);
+    @RequiredArgsConstructor
+    public enum ArmState{
+        STOP(),
+        STOW(),
+        ALGL(),
+        ALGH(),
+        CLVL1(),
+        CLVL2(),
+        ClVL3(),
+        CLVL4(),
+        INTAKE(),
+        PROCESS(),
+        BARGE();
+        private  DoubleSupplier armSetpointSupplier;
+
+        private double getRads(){
+            return Units.degreesToRadians(armSetpointSupplier.getAsDouble());
+        }
+    }
+    @AutoLogOutput(key = "Superstructure/Arm/ArmState")
+    @Getter
+    @Setter
+    private ArmState armState = ArmState.STOW;
+
+    public Arm(ArmIO io) {
+      this.io = io;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/superstructure/arm/ArmIOKrakenx60.java
@@ -0,0 +1,106 @@
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.subsystems.superstructure.arm.ArmIO.ArmIOInputs;
+
+public class ArmIOKrakenx60 implements ArmIO{
+    private final StatusSignal<Angle> positionRotations;
+    private final StatusSignal<Voltage> appliedVolts;
+    private final StatusSignal<AngularVelocity> velocityRadPerSec;
+    private final StatusSignal<Current> supplyCurrentAmps;
+    private final StatusSignal<Current> torqueCurrentAmps;
+    private final StatusSignal<Temperature> tempCelsius;
+    private final TorqueCurrentFOC currentControl = new TorqueCurrentFOC(0.0);
+    private final MotionMagicTorqueCurrentFOC positionControl = new MotionMagicTorqueCurrentFOC(0.0);
+    private final TalonFX armTalon;
+    private final TalonFXConfiguration armConfig;
+
+    public ArmIOKrakenx60(){
+        armTalon = new TalonFX(0);
+        armConfig = new TalonFXConfiguration();
+
+    // armConfig.Slot0.kP = gains.kP();
+    // armConfig.Slot0.kI = gains.kI();
+    // armConfig.Slot0.kD = gains.kD();
+    // armConfig.Slot0.kS = gains.ffkS();
+    // armConfig.Slot0.kV = gains.ffkV();
+    // armConfig.Slot0.kG = gains.ffkG();
+    // armConfig.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+    // armConfig.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseClosedLoopSign;
+    // armConfig.TorqueCurrent.PeakForwardTorqueCurrent = 80.0;
+    // armConfig.TorqueCurrent.PeakReverseTorqueCurrent = -80.0;
+    // armConfig.MotorOutput.Inverted =
+    //     inverted ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+    // armConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    // armConfig.Feedback.FeedbackRemoteSensorID = Constants.getMotorIds().SHOOTER_CANCODER;
+    // armConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+    // armConfig.Feedback.RotorToSensorRatio = armReduction;
+    // armConfig.Feedback.SensorToMechanismRatio = 1.0;
+    // armConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    // armConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = Units.degreesToRotations(maxAngle);
+    // armConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    // armConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = Units.degreesToRotations(minAngle);
+    // armConfig.MotionMagic.MotionMagicAcceleration = cruiseAcceleration;
+    // armConfig.MotionMagic.MotionMagicCruiseVelocity = cruiseVelocity;
+    // armConfig.MotionMagic.MotionMagicJerk = cruiseJerk;
+    // armTalon.getConfigurator().apply(armConfig, 1.0);
+
+    positionRotations = armTalon.getPosition();
+    velocityRadPerSec = armTalon.getVelocity();
+    appliedVolts = armTalon.getMotorVoltage();
+    supplyCurrentAmps = armTalon.getSupplyCurrent();
+    torqueCurrentAmps = armTalon.getTorqueCurrent();
+    tempCelsius = armTalon.getDeviceTemp();
+    BaseStatusSignal.setUpdateFrequencyForAll(0.20, positionRotations,velocityRadPerSec,appliedVolts,supplyCurrentAmps,torqueCurrentAmps,tempCelsius);
+    armTalon.optimizeBusUtilization();
+    
+    }
+
+    @Override
+    public void updateInputs(ArmIOInputs inputs){
+        BaseStatusSignal.refreshAll(positionRotations,appliedVolts,velocityRadPerSec,supplyCurrentAmps,torqueCurrentAmps,tempCelsius);
+        inputs.appliedVolts = appliedVolts.getValueAsDouble();
+        inputs.positionRad = Units.rotationsToRadians(positionRotations.getValueAsDouble());
+        inputs.supplyCurrentAmps = supplyCurrentAmps.getValueAsDouble();
+        inputs.tempCelsius = tempCelsius.getValueAsDouble();
+        inputs.torqueCurrentAmps = torqueCurrentAmps.getValueAsDouble();
+        inputs.velocityRadPerSec = velocityRadPerSec.getValueAsDouble();
+    }
+
+    @Override
+    public void setArmPosition(double desiredPositionRad){
+        armTalon.setControl(positionControl.withPosition(Units.radiansToRotations(desiredPositionRad)));
+    }
+
+    @Override
+    public void runCurrent(double currentSetpoint){
+        armTalon.setControl(currentControl.withOutput(currentSetpoint));
+    }
+
+    @Override
+    public void setBrakeMode(boolean enabled){
+        armTalon.setNeutralMode(enabled ? NeutralModeValue.Brake : NeutralModeValue.Coast);
+    }
+
+    @Override
+    public void setPID(double p, double i, double d){
+        armConfig.Slot0.kP = p;
+        armConfig.Slot0.kI = i;
+        armConfig.Slot0.kP = p;
+        armTalon.getConfigurator().apply(armConfig);
+    }
+
+    
+}
