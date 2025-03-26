1dbf284e839aeed7c2fa173459e70b6d42923542
Noah Schroeder <noahdschroeder@gmail.com>
2025-03-26 12:47:02 -0500

    feat: update robot weight and moi configs

--- src/main/java/frc/robot/subsystem/drive/Drive.java
+++ src/main/java/frc/robot/subsystem/drive/Drive.java
@@ -84,8 +84,8 @@ public class Drive extends SubsystemBase implements AprilTagVision.VisionConsume
               Math.hypot(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)));
 
   // PathPlanner config constants
-  private static final double ROBOT_MASS_KG = 74.088;
-  private static final double ROBOT_MOI = 6.883;
+  private static final double ROBOT_MASS_KG = 62.9994443;
+  private static final double ROBOT_MOI = 6.283;
   private static final double WHEEL_COF = 1.5;
   private static final RobotConfig PP_CONFIG =
       new RobotConfig(
