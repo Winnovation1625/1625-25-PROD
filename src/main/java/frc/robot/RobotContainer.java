a26d99e77d2f1ec92aebeb679313d6244cc3484f
1625-FLOAT2\team1 <team1625@gmail.com>
2025-04-01 16:20:14 -0500

    Update at 'Tue Apr 01 16:20:14 CDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -244,7 +244,7 @@ public class RobotContainer {
         () ->
             AllianceFlipUtil.apply(drive.getPose()).getX()
                     - AllianceFlipUtil.apply(FieldConstants.Barge.middleCage).getX()
-                < 0.4);
+                < 1);
 
     superstructure = new Superstructure(arm, elevator, manipulator::hasAlgae);
     // Set up auto routines
